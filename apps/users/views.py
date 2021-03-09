6c179626b918f11c5a00f5d9c266dbb65cf167e6
john alexander jimenez <jhonalexjm@gmail.com>
2021-03-09 16:47:25 -0500

    fix save data post

--- apps/users/views.py
+++ apps/users/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ('email', 'password', 'is_student')
+        fields = ('email', 'password')
         extra_kwargs = {'password': {'write_only': True, 'min_length': 8}}
 
     def create(self, validated_data):
